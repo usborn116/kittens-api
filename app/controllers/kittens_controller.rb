4d1daf896a7cd1806cde0d1da51aef23bd9f661e
Usborn Ocampo <usborn116@gmail.com>
2022-08-23 09:14:58 -0700

    working controllers, methods, and views

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,47 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to root_path, notice: 'Kitten created!'
+    else
+      render :new, notice: 'Errors!'
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to root_path, notice: 'Kitten updated!'
+    else
+      render :edit, notice: 'Errors!'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_path, notice: 'Kitten deleted!'
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
